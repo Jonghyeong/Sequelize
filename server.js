7e3cbf4c525226500e30024296abb037128fc23c
Jonghyeong Kim <aengida@gmail.com>
2021-03-26 00:27:17 -0400

    set up the livereload

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
+import reload from 'livereload';
+import connectReload from 'connect-livereload';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+// Add some auto-reloading to our server
+const liveReloadServer = reload.createServer();
+liveReloadServer.watch(path.join(__dirname, staticFolder));
+
+// Configure express
+if (process.env.CONTEXT === 'development') {
+  app.use(connectReload());
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 
 async function bootServer() {
@@ -21,6 +32,14 @@ async function bootServer() {
   } catch (err) {
     console.error(err);
   }
+};
+
+if (process.env.CONTEXT === 'development') {
+  liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveReloadServer.refresh('/');
+    }, 100);
+  });
 }
 
 bootServer();
