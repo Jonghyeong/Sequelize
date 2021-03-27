b25f3e8ea6238bb41308df1df9e4fbb3455e25fc
Jonghyeong Kim <aengida@gmail.com>
2021-03-27 00:14:43 -0400

    update 1

---
+++ public/script.js
@@ -0,0 +1,76 @@
+// async function populateRestarants() {
+//     console.log('populate');
+//     const diningRequest = await fetch('/api/dining');
+//     const diningData = await diningRequest.json();
+    
+
+//     diningData.data.forEach((restaurant) => {
+//         const appendItem = document.getElementById('mytable');
+//         appendItem.classList.add('tile', 'has-text-centered', 'is-parent', 'is-3');
+//         appendItem.innerHTML = `
+//             <tr>
+//                 <td class="subtitle has-text-light has-text-weight-bold">
+//                     ${restaurant.hall_id[0]}</td>
+//                 <td class="has-text-light">
+//                     ${restaurant.hall_name[1]}
+//                 </td>
+//                 <td class="has-text-light">
+//                     ${restaurant.hall_address.split(','[2])}
+//                 </td>
+//             </tr>
+//         `;
+//         tagerBox.append(appendItem);        
+//     });
+// }
+
+async function getMeals() {
+    console.log('data request');
+    const diningRequest = await fetch('/api/dining');
+    const diningData = await diningRequest.json();
+    return diningData;
+}
+
+function setBasicData() {
+    localStorage.setItem('myCat', 'Tom');
+}
+
+function getBasicData() {
+    return localStorage.getItem('myCat');
+}
+
+function setComplexData(data) {
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
+async function windowActions() {
+    console.log('loaded window');
+    const meals = await getMeals();
+
+    // setBasicData();
+    // const cat = getBasicData();
+    // console.log(cat);
+    setComplexData(meals);
+    const storedMeals = localStorage.getItem('data');
+    const storedMealdata = JSON.parse(storedMeals);
+    console.log(storedMeals);
+    console.log(storedMealdata);
+
+    storedMealdata.data.forEach(element => {
+        const appendItem = document.getElementById('mytable');
+        appendItem.classList.add('tr');
+        appendItem.innerHTML = `<tr>
+                        <td class="subtitle has-text-light has-text-weight-bold">
+                            ${element.hall_id[0]}</td>
+                        <td class="has-text-light">
+                            ${element.hall_name[1]}
+                        </td>
+                        <td class="has-text-light">
+                            ${element.hall_address.split(','[2])}
+                        </td>
+                    </tr>
+        `;        
+    });
+
+}
+
+window.onload = windowActions;
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
-import reload from 'livereload';
-import connectReload from 'connect-livereload';
+// import reload from 'livereload';
+// import connectReload from 'connect-livereload';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-// Add some auto-reloading to our server
-const liveReloadServer = reload.createServer();
-liveReloadServer.watch(path.join(__dirname, staticFolder));
 
-// Configure express
-if (process.env.CONTEXT === 'development') {
-  app.use(connectReload());
-}
+// Add some auto-reloading to our server
+// const liveReloadServer = reload.createServer();
+// liveReloadServer.watch(path.join(__dirname, staticFolder));
+
+// // Configure express
+// if (process.env.CONTEXT === 'development') {
+//   app.use(connectReload());
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
