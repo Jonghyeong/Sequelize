de6e21b1aa424a8c22b3dc1a3041b322cf186945
Jonghyeong Kim <aengida@gmail.com>
2021-03-29 15:06:55 -0400

    update5

--- public/script1.js
+++ public/script1.js
@@ -1,26 +1,34 @@
-async function populateRestarants() {
+async function populateRestarants(diningData) {
     console.log('populate');
+    const table = document.getElementById('mytable');
+    
+    diningData.data.forEach((restaurant) => {
+        
+        const appendItem = document.createElement('tr');
+
+        appendItem.innerHTML = `
+                <td>${restaurant.hall_id}</td>
+                <td>${restaurant.hall_name}</td>
+                <td>${restaurant.hall_address}</td>
+            `;
+
+        table.append(appendItem);        
+    });
+}
+
+async function getDining() {
+    console.log('data request');
     const diningRequest = await fetch('/api/dining');
     const diningData = await diningRequest.json();
-    
+    return diningData;
+}
 
-    diningData.data.forEach((restaurant) => {
-        const appendItem = document.getElementById('mytable');
-        appendItem.classList.add('tile', 'has-text-centered', 'is-parent', 'is-3');
-        appendItem.innerHTML = `
-            <tr>
-                <td class="subtitle has-text-light has-text-weight-bold">
-                    ${restaurant.hall_id[0]}</td>
-                <td class="has-text-light">
-                    ${restaurant.hall_name[1]}
-                </td>
-                <td class="has-text-light">
-                    ${restaurant.hall_address.split(','[2])}
-                </td>
-            </tr>
-        `;
-        tagerBox.append(appendItem);        
-    });
+function setBasicData() {
+    localStorage.setItem('myCat', 'Tom');
+}
+
+function getBasicData() {
+    return localStorage.getItem('myCat');
 }
 
 function setComplexData(data) {
@@ -29,13 +37,14 @@ function setComplexData(data) {
 
 async function windowActions() {
     console.log('loaded window');
-    const meals = await getMeals();
+    const meals = await getDining();
 
     setComplexData(meals);
     const storedMeals = localStorage.getItem('data');
     const storedMealdata = JSON.parse(storedMeals);
     console.log(storedMeals);
     console.log(storedMealdata);
+    populateRestarants(storedMealdata);
 }
 
 window.onload = windowActions;
